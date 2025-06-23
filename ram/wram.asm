342239c553f300a8a862e35558f9822cc46b61cc
CreamElDudJafar <jahern2398@gmail.com>
2025-06-23 10:22:06 -0500

    Post Game Nurse Joy and Officer Jenny Fights
    More Post Game Content:
    
    Gen 2 Picture Credit: Rosie and FrenchOrange
    
    Gen 1 Picture Credit: YL + ZuperZach

--- ram/wram.asm
+++ ram/wram.asm
@@ -2182,7 +2182,7 @@ wLancesRoomCurScript:: db
 	ds 2
 wSilphCo10FCurScript:: db
 wSilphCo11FCurScript:: db
-	ds 1
+wFuchsiaPokecenterCurScript:: db
 wFuchsiaGymCurScript:: db
 wSaffronGymCurScript:: db
 	ds 1
