c42e9aace6b2cc7c95e11ef3b9f7740437158154
CreamElDudJafar <jahern2398@gmail.com>
2025-06-29 10:53:03 -0500

    Speed Up HP Bar Speed Going Down

--- engine/gfx/hp_bar.asm
+++ engine/gfx/hp_bar.asm
@@ -144,7 +144,7 @@ UpdateHPBar_AnimateHPBar:
 	push de
 	ld d, $6
 	call DrawHPBarWithColor ; HAX: call changed to update color as well
-	ld c, 2
+	ld c, 1
 	rst _DelayFrames
 	pop de
 	ld a, [wHPBarDelta] ; +1 or -1
