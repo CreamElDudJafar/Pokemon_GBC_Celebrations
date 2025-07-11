3c961ef55870e4b386e80e9e102f86a8d81a8e7e
CreamElDudJafar <jahern2398@gmail.com>
2025-07-11 08:41:28 -0500

    Fix Start Menu not wrapping back to the top without the Pokedex and Town Map

--- home/start_menu.asm
+++ home/start_menu.asm
@@ -26,10 +26,12 @@ RedisplayStartMenu_DoNotDrawStartMenu::
 	jr nz, .loop
 ; if the player pressed tried to go past the top item, wrap around to the bottom
 	CheckEvent EVENT_GOT_TOWN_MAP
-	ld a, 7 ; there are 8 menu items with the pokedex, so the max index is 6
+	ld a, 7 ; there are 8 menu items with the town map, so the max index is 7
 	jr nz, .wrapMenuItemId
 	CheckEvent EVENT_GOT_POKEDEX
-	ld a, 6 ; there are only 7 menu items without the pokedex
+	ld a, 6 ; there are only 7 menu items with the pokedex, , so the max index is 6
+	jr nz, .wrapMenuItemId
+	dec a ; there are only 6 menu items without the pokedex
 .wrapMenuItemId
 	ld [wCurrentMenuItem], a
 	call EraseMenuCursor
@@ -80,6 +82,7 @@ RedisplayStartMenu_DoNotDrawStartMenu::
 	jp z, StartMenu_SaveReset
 	cp 5
 	jp z, StartMenu_Option
+; Check for Town Map
 	ld b, a
 	CheckEvent EVENT_GOT_TOWN_MAP
 	ld a, b
