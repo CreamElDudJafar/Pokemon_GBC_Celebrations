1d30873ffa464cec21d13c8ebbcf91bf8fa69fbd
CreamElDudJafar <jahern2398@gmail.com>
2025-09-08 08:18:36 -0500

    Add Pokemon Observer/Coin Giver in Celadon Hotel
    Pokemon Must Be in First Slot. Changes if you exit hotel or save game and power back on
    
    Credit: Jojobear13/ShinPokeRed

--- main.asm
+++ main.asm
@@ -424,6 +424,8 @@ INCLUDE "engine/gfx/animated_tiles_code.asm"
 INCLUDE "engine/battle/just_slot_machine_stuff.asm" ; free some space for bank1E
 INCLUDE "engine/menus/option_sound_test.asm"
 INCLUDE "engine/overworld/specific_script_flags.asm"
+INCLUDE "engine/custom_function/coin_function.asm"
+INCLUDE "engine/custom_function/monlist.asm"
 
 
 SECTION "Evo Moves", ROMX
