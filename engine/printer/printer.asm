990af5a597bf46a44e6762f43c01ce2c48962230
CreamElDudJafar <jahern2398@gmail.com>
2025-09-28 18:48:07 -0500

    Update Printer Music Settings
    And comments

--- engine/printer/printer.asm
+++ engine/printer/printer.asm
@@ -445,24 +445,11 @@ Printer_ResetJoypadHRAM:
 	ldh [hJoy6], a
 	ret
 
-;Printer_PlayPrinterMusic:
-;	call Printer_FadeOutMusicAndWait
-;	ld a, [wAudioROMBank]
-;	ld [wAudioSavedROMBank], a
-;	ld a, BANK(Music_GBPrinter)
-;	ld [wAudioROMBank], a
-;	ld a, MUSIC_GB_PRINTER
-;	ld [wNewSoundID], a
-;	rst _PlaySound
-;	ret
-
-;The audio banks are already crammed pretty full,
-;so use something that is already in the game for less of a headache
 Printer_PlayPrinterMusic:
 	call Printer_FadeOutMusicAndWait
 ;	ld a, [wAudioROMBank]
 ;	ld [wAudioSavedROMBank], a
-;	ld a, 0 ; BANK(Music_MuseumGuy)
+;	ld a, 0 ; BANK(Music_GBPrinter)
 ;	ld [wAudioROMBank], a
 	ld a, MUSIC_GB_PRINTER
 ;	ld [wNewSoundID], a
