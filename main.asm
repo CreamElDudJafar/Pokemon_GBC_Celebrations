1412fac2f7b449bf73c64248adf2ff81850cf511
CreamElDudJafar <jahern2398@gmail.com>
2026-01-22 20:27:38 -0600

    Removed Unused File
    Nothing in it

--- main.asm
+++ main.asm
@@ -95,7 +95,7 @@ INCLUDE "engine/overworld/is_player_just_outside_map.asm"
 INCLUDE "engine/pokemon/status_screen.asm"
 INCLUDE "engine/menus/party_menu.asm"
 INCLUDE "gfx/player.asm"
-INCLUDE "engine/overworld/turn_sprite.asm"
+;INCLUDE "engine/overworld/turn_sprite.asm" ; removed
 INCLUDE "engine/menus/start_sub_menus.asm"
 INCLUDE "engine/items/tms.asm"
 INCLUDE "engine/battle/end_of_battle.asm"
